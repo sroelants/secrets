fffbb78a025bcbeef9f8f08e199043683786dd95
Sam Roelants <roelants.sam@gmail.com>
2019-10-28 22:31:33 +0100

    Updates

---
+++ precache-manifest.293ab7431a5003e25e591d2313841681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19af9838cda2c4a01e422e026d49602e",
+    "url": "/secrets/index.html"
+  },
+  {
+    "revision": "85f32f69cda0de014a8a",
+    "url": "/secrets/static/css/main.48373b24.chunk.css"
+  },
+  {
+    "revision": "9823572d6df0a083671a",
+    "url": "/secrets/static/js/2.9d51a6b0.chunk.js"
+  },
+  {
+    "revision": "85f32f69cda0de014a8a",
+    "url": "/secrets/static/js/main.dd1a22a4.chunk.js"
+  },
+  {
+    "revision": "d68ee77f9168eace52d8",
+    "url": "/secrets/static/js/runtime-main.2b24b0ac.js"
+  }
+]);
